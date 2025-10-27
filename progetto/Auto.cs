b7ef6b9945f33eba0e9d74a5a191ef1f45f7c9a8
Rebecca <rebecca.ravagli@studenti.isissgobetti.it>
2025-10-27 12:31:30 +0100

    creazione classi Auto e Veicolo

---
+++ progetto/Auto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progetto
+{
+    internal class Auto : Veicolo
+    {
+        private int nposti;
+
+        public Auto(int nposti, string marca, string modello, int anno) :base(marca,modello,anno)
+        {
+            this.nposti = nposti;
+        }
+
+    }
+}
---
+++ progetto/Veicolo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progetto
+{
+    internal class Veicolo
+    {
+        protected string marca;
+        protected string modello;
+        protected int anno;
+        protected bool acceso;
+
+        public Veicolo(string marca, string modello, int anno)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.anno = anno;
+            acceso = false;
+        }
+
+        private bool OnOff()
+        {
+            acceso=!acceso;
+            return acceso;
+        }
+
+    }
+}
